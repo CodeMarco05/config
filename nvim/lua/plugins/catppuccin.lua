86fd96fb760f23543c342dd437af87c6847715a9
marco <marco@fedora.fritz.box>
2025-02-24 23:26:45 +0100

    nvim updates

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,20 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		--vim.cmd.colorscheme("catppuccin")
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,    -- Load immediately
+  priority = 1000, -- Load before other plugins
+  config = function()
+    require("catppuccin").setup({
+      flavor = "macchiato", -- Options: latte, frappe, macchiato, mocha
+      integrations = {
+        treesitter = true,
+        cmp = true,
+        gitsigns = true,
+        telescope = true,
+        notify = false,
+      },
+    })
+    --vim.cmd.colorscheme("catppuccin") -- Apply the theme
+  end,
 }
+
