bc4b6cde495c916e00eab5e99c68cd852c497831
CodeMarco05 <brandt.marco05@gmail.com>
2025-11-11 13:56:50 +0100

    plugin update, lsp and lazygit

--- nvim/lua/plugins/alpha-vim.lua
+++ nvim/lua/plugins/alpha-vim.lua
@@ -1,7 +1,20 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = { "echasnovski/mini.icons" },
-	config = function()
-		require("alpha").setup(require("alpha.themes.startify").config)
-	end,
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "echasnovski/mini.icons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"startup-nvim/startup.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		config = function()
+			-- require("startup").setup()
+		end,
+	},
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -14,7 +14,7 @@ return {
 					"rust_analyzer",
 					"marksman",
 					"clangd",
-          "jdtls",
+					"jdtls",
 				},
 			})
 		end,
@@ -24,25 +24,43 @@ return {
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			-- Set global capabilities for all servers
-			vim.lsp.config('*', {
+			local lspconfig = require("lspconfig")
+			
+			-- Setup each server with proper configuration
+			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
-
-			-- Enable LSP servers
-			vim.lsp.enable('lua_ls')
-			vim.lsp.enable('dockerls')
-			vim.lsp.enable('docker_compose_language_service')
-			vim.lsp.enable('rust_analyzer')
-			vim.lsp.enable('clangd')
-			vim.lsp.enable('jdtls')
-
+			
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+			})
+			
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			
+			lspconfig.docker_compose_language_service.setup({
+				capabilities = capabilities,
+			})
+			
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+			
 			-- Key mappings for LSP functions
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { noremap = true, silent = true }) -- Go to Definition
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true }) -- Rename
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true })
 		end,
 	},
 }
