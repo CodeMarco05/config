a99fd67102e1df3f615604cbbb1d0d6045d95885
Marco <brandt.marco05@gmail.com>
2025-11-11 23:09:01 +0100

    changed lsp config to newer suport

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -24,38 +24,47 @@ return {
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			
-			-- Setup each server with proper configuration
-			lspconfig.lua_ls.setup({
+			-- Setup each server with proper configuration using the new API
+			vim.lsp.config.lua_ls = {
 				capabilities = capabilities,
-			})
-			
-			lspconfig.rust_analyzer.setup({
+			}
+
+			vim.lsp.config.rust_analyzer = {
 				capabilities = capabilities,
-			})
-			
-			lspconfig.clangd.setup({
+			}
+
+			vim.lsp.config.clangd = {
 				capabilities = capabilities,
 				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-			})
-			
-			lspconfig.jdtls.setup({
+			}
+
+			vim.lsp.config.jdtls = {
 				capabilities = capabilities,
-			})
-			
-			lspconfig.dockerls.setup({
+			}
+
+			vim.lsp.config.dockerls = {
 				capabilities = capabilities,
-			})
-			
-			lspconfig.docker_compose_language_service.setup({
+			}
+
+			vim.lsp.config.docker_compose_language_service = {
 				capabilities = capabilities,
-			})
-			
-			lspconfig.marksman.setup({
+			}
+
+			vim.lsp.config.marksman = {
 				capabilities = capabilities,
+			}
+
+			-- Enable the servers
+			vim.lsp.enable({
+				"lua_ls",
+				"rust_analyzer",
+				"clangd",
+				"jdtls",
+				"dockerls",
+				"docker_compose_language_service",
+				"marksman",
 			})
-			
+
 			-- Key mappings for LSP functions
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
