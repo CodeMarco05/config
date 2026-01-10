ab6621a17fe2ef8c69da93fe48e0c89f8a847c7c
Marco <brandt.marco05@gmail.com>
2026-01-10 13:37:31 +0100

    copilot plugin

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,23 @@
+return {
+	-- GitHub Copilot
+	{
+		"github/copilot.vim",
+		event = "InsertEnter",
+		config = function()
+			-- Disable default tab mapping (optional, if you want custom keybindings)
+			vim.g.copilot_no_tab_map = true
+
+			-- Set Copilot to use specific Node.js version (optional)
+			-- vim.g.copilot_node_command = "/path/to/node"
+
+			-- Filetypes to disable Copilot
+			vim.g.copilot_filetypes = {
+				["*"] = true,
+				["gitcommit"] = false,
+				["gitrebase"] = false,
+				["markdown"] = true,
+				[".env"] = false,
+			}
+		end,
+	},
+}
